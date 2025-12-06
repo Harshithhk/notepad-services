c9ff535cd35da3a8d92f236a3fc2cb0cfc7fae86
Tolaj <swapnilhgf@gmail.com>
2025-12-06 15:33:16 -0500

    add: signin and signup page

--- app/frontend-service/src/App.jsx
+++ app/frontend-service/src/App.jsx
@@ -1,11 +1,5 @@
+import AppRouter from "./router";
 
-
-const App = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+export default function App() {
+  return <AppRouter />;
 }
-
-export default App;
