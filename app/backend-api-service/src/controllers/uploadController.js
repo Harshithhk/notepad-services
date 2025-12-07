3c1f020037ce38e62118bfa32fc1663eb89e8b85
Harshithhk <dota2kelkar@gmail.com>
2025-12-07 18:10:31 -0500

    removed unnecessary credential imports for s4

--- app/backend-api-service/src/controllers/uploadController.js
+++ app/backend-api-service/src/controllers/uploadController.js
@@ -1,46 +1,40 @@
-
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { v4 as uuidv4 } from "uuid";
 import { S3Client } from "@aws-sdk/client-s3";
 
-
-
 export const imageUpload = async (req, res) => {
-    try {
-        const s3 = new S3Client({
-            region: process.env.AWS_REGION,
-            credentials: {
-                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-            }
-        });
-        const { fileType } = req.body;
+  try {
+    const s3 = new S3Client({
+      region: process.env.AWS_REGION,
+    });
 
-        if (!fileType) {
-            return res.status(400).json({ error: "fileType required" });
-        }
-
-        const fileKey = `uploads/${uuidv4()}`;
-
-        const command = new PutObjectCommand({
-            Bucket: process.env.S3_BUCKET_NAME,
-            Key: fileKey,
-            // ContentType: fileType
-        });
-        const uploadUrl = await getSignedUrl(s3, command, {
-            expiresIn: 300
-        });
-
-        const fileUrl = `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileKey}`;
-
-        res.json({
-            uploadUrl,
-            fileUrl,
-            key: fileKey
-        });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "Failed to generate presigned URL" });
+    const { fileType } = req.body;
+    if (!fileType) {
+      return res.status(400).json({ error: "fileType required" });
     }
-}
+
+    const fileKey = `uploads/${uuidv4()}`;
+
+    const command = new PutObjectCommand({
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: fileKey,
+      ContentType: fileType,
+    });
+
+    const uploadUrl = await getSignedUrl(s3, command, {
+      expiresIn: 300,
+    });
+
+    const fileUrl = `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileKey}`;
+
+    res.json({
+      uploadUrl,
+      fileUrl,
+      key: fileKey,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to generate presigned URL" });
+  }
+};
