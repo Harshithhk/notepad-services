291c20f3986a78c28c24fffefd0c7b9980715ed2
Tolaj <swapnilhgf@gmail.com>
2025-12-07 21:32:26 -0500

    add:dialog box

--- app/frontend-service/src/pages/Dashboard.jsx
+++ app/frontend-service/src/pages/Dashboard.jsx
@@ -6,12 +6,22 @@ import mainLogo from '../assets/logo.png'
 import { noteService } from "../services/noteService";
 import { AuthContext } from "../context/AuthContext";
 
+
+import JsonView from '@uiw/react-json-view';
+
+import { vscodeTheme } from '@uiw/react-json-view/vscode';
+import { gruvboxTheme } from '@uiw/react-json-view/gruvbox';
+import { monokaiTheme } from '@uiw/react-json-view/monokai';
+import { basicTheme } from '@uiw/react-json-view/basic';
+
 export default function Dashboard() {
     const [isOpen, setIsOpen] = useState(false);
     const [mode, setMode] = useState(null);
     const [image, setImage] = useState(null);
     const webcamRef = useRef(null);
     const [notes, setNotes] = useState([]);
+    const [showModal, setShowModal] = useState(0);
+    const [modalNote, setModalNote] = useState(0);
 
     const { user, setUser, logout } = useContext(AuthContext);
 
@@ -20,6 +30,20 @@ export default function Dashboard() {
         const imageSrc = webcamRef.current.getScreenshot();
         setImage(imageSrc);
     };
+
+    const handleModalNote = async (note) => {
+        setShowModal(1)
+        let tempNote = note
+        delete tempNote.userId
+        delete tempNote._id
+        delete tempNote.interpretationCompleted
+        delete tempNote.createdAt
+        delete tempNote.updatedAt
+        delete tempNote.__v
+        delete tempNote.embedding
+
+        setModalNote(tempNote)
+    }
 
     const handleUploadFile = (e) => {
         const file = e.target.files[0];
@@ -103,7 +127,7 @@ export default function Dashboard() {
             </div>
 
             {/* table header */}
-            <div class="max-w-screen-xl px-4 mx-auto lg:px-12 w-full">
+            <div class="max-w-screen-xl px-4 mx-auto lg:px-12 w-full mb-6">
                 <div class="relative bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
                     <div class="flex flex-col items-center justify-between p-4 space-y-3 md:flex-row md:space-y-0 md:space-x-4">
                         <div class="w-full md:w-1/2">
@@ -117,6 +141,15 @@ export default function Dashboard() {
                                     </div>
                                     <input type="text" id="simple-search" class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search" required="" />
                                 </div>
+                                <button type="button" class="flex items-center gap-2 mx-2 justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
+
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5">
+                                        <path fill-rule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clip-rule="evenodd" />
+                                    </svg>
+
+
+                                    Search
+                                </button>
                             </form>
                         </div>
                         <div class="flex flex-col items-stretch justify-end flex-shrink-0 w-full space-y-2 md:w-auto md:flex-row md:space-y-0 md:items-center md:space-x-3">
@@ -129,68 +162,7 @@ export default function Dashboard() {
 
                                 Capture notes
                             </button>
-                            <div class="flex items-center w-full space-x-3 md:w-auto">
-                                <button id="actionsDropdownButton" data-dropdown-toggle="actionsDropdown" class="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg md:w-auto focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" type="button">
-                                    <svg class="-ml-1 mr-1.5 w-5 h-5" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                        <path clip-rule="evenodd" fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
-                                    </svg>
-                                    Actions
-                                </button>
-                                <div id="actionsDropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-                                    <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="actionsDropdownButton">
-                                        <li>
-                                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Mass Edit</a>
-                                        </li>
-                                    </ul>
-                                    <div class="py-1">
-                                        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete all</a>
-                                    </div>
-                                </div>
-                                <button id="filterDropdownButton" data-dropdown-toggle="filterDropdown" class="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg md:w-auto focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" type="button">
-                                    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="w-4 h-4 mr-2 text-gray-400" viewbox="0 0 20 20" fill="currentColor">
-                                        <path fill-rule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clip-rule="evenodd" />
-                                    </svg>
-                                    Filter
-                                    <svg class="-mr-1 ml-1.5 w-5 h-5" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                        <path clip-rule="evenodd" fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
-                                    </svg>
-                                </button>
-                                <div id="filterDropdown" class="z-10 hidden w-48 p-3 bg-white rounded-lg shadow dark:bg-gray-700">
-                                    <h6 class="mb-3 text-sm font-medium text-gray-900 dark:text-white">
-                                        Category
-                                    </h6>
-                                    <ul class="space-y-2 text-sm" aria-labelledby="dropdownDefault">
-                                        <li class="flex items-center">
-                                            <input id="apple" type="checkbox" value=""
-                                                class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                                            <label for="apple" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
-                                                Apple (56)
-                                            </label>
-                                        </li>
-                                        <li class="flex items-center">
-                                            <input id="fitbit" type="checkbox" value=""
-                                                class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                                            <label for="fitbit" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
-                                                Fitbit (56)
-                                            </label>
-                                        </li>
-                                        <li class="flex items-center">
-                                            <input id="dell" type="checkbox" value=""
-                                                class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                                            <label for="dell" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
-                                                Dell (56)
-                                            </label>
-                                        </li>
-                                        <li class="flex items-center">
-                                            <input id="asus" type="checkbox" value="" checked
-                                                class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                                            <label for="asus" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
-                                                Asus (97)
-                                            </label>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
+
                         </div>
                     </div>
                 </div>
@@ -199,7 +171,7 @@ export default function Dashboard() {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                 {notes.length > 0 ? (
                     notes.map((note) => (
-                        <div key={note._id} className="border rounded-lg p-4 shadow-sm">
+                        <div onClick={() => handleModalNote(note)} key={note._id} className="border rounded-lg p-4 shadow-sm">
                             <img
                                 src={note.imageUrl}
                                 alt={note.title || "note"}
@@ -327,6 +299,32 @@ export default function Dashboard() {
                     </Dialog.Panel>
                 </div>
             </Dialog>
+
+            <div id="default-modal" tabindex="-1" aria-hidden="true" class={`${showModal === 1 ? '' : 'hidden'} bg-black bg-opacity-50 flex items-center overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center  w-full md:inset-0 h-full max-h-full`}>
+                <div class="relative p-4 w-full max-w-6xl h-[85%]  max-h-full bg-white">
+                    <div class="relative bg-neutral-primary-soft border border-default rounded-base shadow-sm p-4 md:p-6 mb-4">
+                        <div class="flex items-center justify-between  ">
+                            <h2 className="font-bold text-lg mb-1">{modalNote.title || "Untitled"}</h2>
+
+                            <button onClick={() => { setShowModal(0) }} type="button" class="text-body bg-transparent hover:bg-neutral-tertiary hover:text-heading rounded-base text-sm w-9 h-9 ms-auto inline-flex justify-center items-center" data-modal-hide="default-modal">
+                                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6" /></svg>
+                                <span class="sr-only">Close modal</span>
+                            </button>
+                        </div>
+
+                    </div>
+                    <div className=" flex items-center justify-evenly overflow-y-scroll h-70">
+                        <img
+                            src={modalNote.imageUrl}
+                            alt={modalNote.title || "note"}
+                            className="w-70 h-48 object-cover rounded-md mb-2"
+                        />
+                        <JsonView value={modalNote} style={vscodeTheme} />
+
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
